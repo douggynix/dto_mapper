55552b9d983101eca3406e9b2192577ee345bf4c
James Douglass L. <douggynix@gmail.com>
2024-11-16 19:24:52 +0100

    code refactoring changes with macro attributes added to mapped existing fields (#12)
    * code refactoring changes with properties added to mapped fields
    
    * code changes for previous review
    
    * remove unnecessary import prefix
    
    * bump cargo dependencies
    
    * adding custom macro attributes for mapping fields
    
    * adding rustfmt.toml for default source code formatting config
    
    * replacing unstrigify by syn parse_sr
    
    * update dto example with custom attributes
    
    * Update README.md
    
    * Fix README.md typo
    
    * update README.md with dependencies removal
    
    * adjust README.md with more detail explanation
    
    * bump dto_mapper version from 0.2.0 to 0.3.0
    Adding change log to track features and history

--- examples/dto_example.rs
+++ examples/dto_example.rs
@@ -1,12 +1,12 @@
 use derive_builder::Builder;
 use dto_mapper::DtoMapper;
+use serde::{Deserialize, Serialize};
 #[allow(unused)]
 use std::str::FromStr;
 #[allow(unused)]
-use unstringify::unstringify;
 
 fn concat_str(s1: &str, s2: &str) -> String {
-  s1.to_owned() + " " + s2
+    s1.to_owned() + " " + s2
 }
 
 #[derive(DtoMapper, Debug, Default, Clone)]
@@ -16,24 +16,47 @@ fn concat_str(s1: &str, s2: &str) -> String {
 #[mapper( dto="CustomDto" , no_builder=true , map=[ ("email",false) ] , derive=(Debug, Clone) ,
   new_fields=[( "name: String", "concat_str( self.firstname.as_str(), self.lastname.as_str() )" )]
 )]
+#[mapper(
+    dto="CustomDtoWithAttribute" ,
+    no_builder=true ,
+    map=[ ("email", false, ["#[serde(rename = \"email_address\")]"] ) ],
+    derive=(Debug, Clone, Serialize, Deserialize),
+    new_fields=[
+        (
+            "name: String",
+            "concat_str( self.firstname.as_str(), self.lastname.as_str() )",
+            ["#[serde(rename = \"full_name\")]"],
+        ),
+        (
+          "hidden_password: String",
+         r#""*".repeat( self.password.len() )"#
+        ),
+    ],
+    macro_attr=["serde(rename_all = \"UPPERCASE\")"]
+)]
 struct User {
-  username: String,
-  password: String,
-  email: String,
-  firstname: String,
-  middle_name: Option<String>,
-  lastname: String,
-  age: u8,
+    username: String,
+    password: String,
+    email: String,
+    firstname: String,
+    middle_name: Option<String>,
+    lastname: String,
+    age: u8,
 }
 
 fn main() {
-  let user = User {
-    firstname: "Dessalines".into(),
-    lastname: "Jean Jacques".into(),
-    ..User::default()
-  };
+    let user = User {
+        firstname: "Dessalines".into(),
+        lastname: "Jean Jacques".into(),
+        email: "dessalines@gmail.com".into(),
+        password: "password123".into(),
+        ..User::default()
+    };
 
-  println!("{:?}", user);
-  let custom_dto: CustomDto = user.into();
-  println!("{:?}", custom_dto);
+    println!("{:?}", user);
+    let custom_dto: CustomDto = user.clone().into();
+    println!("{:?}", custom_dto);
+
+    let custom_dto_attributes: CustomDtoWithAttribute = user.clone().into();
+    println!("{:?}", custom_dto_attributes);
 }
